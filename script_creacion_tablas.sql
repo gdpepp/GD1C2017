070f9794925b82d6efa9e497f40e628f5f163417
Pablo <pnmorrone@gmail.com>
2017-04-21 18:15:45 -0300

    Agrego Todas las tablas que faltaban conectadas a las que ya existian

--- script_creacion_tablas.sql
+++ script_creacion_tablas.sql
@@ -1,6 +1,7 @@
 USE [GD1C2017]
 GO
 
+/****** Object:  Table [FSOCIETY].[Autos]    Script Date: 21/04/2017 12:36:03 p.m. ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,112 +11,348 @@ GO
 SET ANSI_PADDING ON
 GO
 
-CREATE SEQUENCE [gd_esquema].[FSOCIETY.RolesIDSeq]
-	START WITH 1
-	INCREMENT BY 1
-	NO CACHE
-	;
-GO
+----------------------------------------
+--Roles
+----------------------------------------
 
-CREATE TABLE [gd_esquema].[FSOCIETY.Roles](
-	[ID] [int] NOT NULL PRIMARY KEY DEFAULT (NEXT VALUE FOR [gd_esquema].[FSOCIETY.RolesIDSeq]),
-	[Descripcion] [varchar](50) NOT NULL UNIQUE,
-	[Habilitado] [Bit] NOT NULL DEFAULT 1,
-) ON [PRIMARY]
-
-GO
-
-CREATE SEQUENCE [gd_esquema].[FSOCIETY.FuncionalidadesIDSeq]
-	START WITH 1
-	INCREMENT BY 1
-	NO CACHE
-	;
-GO
-
-CREATE TABLE [gd_esquema].[FSOCIETY.Funcionalidades](
-	[ID] [int] NOT NULL PRIMARY KEY DEFAULT (NEXT VALUE FOR [gd_esquema].[FSOCIETY.FuncionalidadesIDSeq]),
-	[Descripcion] [varchar](50) NOT NULL UNIQUE,
-) ON [PRIMARY]
-
-GO
-
-CREATE TABLE [gd_esquema].[FSOCIETY.RolFuncionalidades](
-	[IDRol] [int] NOT NULL FOREIGN KEY REFERENCES [gd_esquema].[FSOCIETY.Roles](ID), 
-	[IDFuncionalidad] [int] NOT NULL FOREIGN KEY REFERENCES [gd_esquema].[FSOCIETY.Funcionalidades](ID),
-	PRIMARY KEY ([IDRol], [IDFuncionalidad])
-) ON [PRIMARY]
-
-GO
-
-CREATE TABLE [gd_esquema].[FSOCIETY.UruarioRoles](
-	[IDUsuario] [int] NOT NULL FOREIGN KEY REFERENCES [gd_esquema].[FSOCIETY.Usuarios](ID), 
-	[IDRol] [int] NOT NULL FOREIGN KEY REFERENCES [gd_esquema].[FSOCIETY.Roles](ID),
-) ON [PRIMARY]
-
-/****** Object:  Table [gd_esquema].[FSOCIETY.Clientes]    Script Date: 4/20/2017 10:05:17 PM ******/
-CREATE TABLE [gd_esquema].[FSOCIETY.Clientes](
-	[ID] [int] NOT NULL,
-	[Nombre] [varchar](50) NOT NULL,
-	[Apellido] [varchar](50) NOT NULL,
-	[DNI] [int] NOT NULL,
-	[Mail] [varchar](50) NULL,
-	[Telefono] [int] NOT NULL,
-	[Direccion] [varchar](100) NOT NULL,
-	[CodigoPostal] [int] NOT NULL,
-	[FechaDeNacimiento] [smalldatetime] NOT NULL,
-	[Inhabilitado] [bit] NOT NULL,
- CONSTRAINT [PK_FSOCIETY.Clientes] PRIMARY KEY CLUSTERED 
+CREATE TABLE [FSOCIETY].[Roles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](50) NOT NULL,
+	[Habilitado] [bit] NOT NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
 (
-	[ID] ASC
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+----------------------------------------
+--Funcionalidades
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Funcionalidades](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descripcion] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Funcionalidades] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+----------------------------------------
+--RolFuncionalidades
+----------------------------------------
+CREATE TABLE [FSOCIETY].[RolFuncionalidades](
+	[IdRol] [int] NOT NULL,
+	[IdFuncionalidad] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[RolFuncionalidades]  WITH CHECK ADD  CONSTRAINT [FK_RolFuncionalidades_Funcionalidades] FOREIGN KEY([IdFuncionalidad])
+REFERENCES [FSOCIETY].[Funcionalidades] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[RolFuncionalidades] CHECK CONSTRAINT [FK_RolFuncionalidades_Funcionalidades]
+GO
+
+ALTER TABLE [FSOCIETY].[RolFuncionalidades]  WITH CHECK ADD  CONSTRAINT [FK_RolFuncionalidades_Roles] FOREIGN KEY([IdRol])
+REFERENCES [FSOCIETY].[Roles] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[RolFuncionalidades] CHECK CONSTRAINT [FK_RolFuncionalidades_Roles]
+GO
+
+----------------------------------------
+--Direccion
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Direccion](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Calle] [varchar](100) NOT NULL,
+	[Altura] [varchar](10) NOT NULL,
+	[Piso] [nchar](10) NULL,
+	[Dpto] [nchar](10) NULL,
+	[Localidad] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_Direccion] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+----------------------------------------
+--Personas
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Personas](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[Apellido] [varchar](100) NOT NULL,
+	[DNI] [varchar](8) NOT NULL,
+	[IdDireccion] [int] NOT NULL,
+	[Fecha de Nacimiento] [smalldatetime] NOT NULL,
+ CONSTRAINT [PK_Personas] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
 
-/****** Object:  Table [gd_esquema].[FSOCIETY.Facturacion]    Script Date: 4/20/2017 10:05:28 PM ******/
-CREATE TABLE [gd_esquema].[FSOCIETY.Facturacion](
-	[ID] [int] NOT NULL,
-	[IDClientes] [int] NOT NULL,
-	[IDViaje] [int] NOT NULL,
+ALTER TABLE [FSOCIETY].[Personas]  WITH CHECK ADD  CONSTRAINT [FK_Persona_Direccion] FOREIGN KEY([IdDireccion])
+REFERENCES [FSOCIETY].[Direccion] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Personas] CHECK CONSTRAINT [FK_Persona_Direccion]
+GO
+
+----------------------------------------
+--Usuarios
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Usuarios](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](30) NOT NULL,
+	[Password] [varbinary](50) NOT NULL,
+	[IdPersona] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Usuarios]  WITH CHECK ADD  CONSTRAINT [FK_Usuarios_Personas] FOREIGN KEY([IdPersona])
+REFERENCES [FSOCIETY].[Personas] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Usuarios] CHECK CONSTRAINT [FK_Usuarios_Personas]
+GO
+----------------------------------------
+--UsuariosRoles
+----------------------------------------
+CREATE TABLE [FSOCIETY].[UsuariosRoles](
+	[IdUsuario] [int] NOT NULL,
+	[IdRol] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[UsuariosRoles]  WITH CHECK ADD  CONSTRAINT [FK_UsuariosRoles_Roles] FOREIGN KEY([IdRol])
+REFERENCES [FSOCIETY].[Roles] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[UsuariosRoles] CHECK CONSTRAINT [FK_UsuariosRoles_Roles]
+GO
+
+ALTER TABLE [FSOCIETY].[UsuariosRoles]  WITH CHECK ADD  CONSTRAINT [FK_UsuariosRoles_Usuarios] FOREIGN KEY([IdUsuario])
+REFERENCES [FSOCIETY].[Usuarios] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[UsuariosRoles] CHECK CONSTRAINT [FK_UsuariosRoles_Usuarios]
+GO
+----------------------------------------
+--Chofer
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Chofer](
+	[Id] [int] NOT NULL,
+	[Telefono] [varchar](50) NOT NULL,
+	[Email] [varchar](100) NULL,
+	[Habilitado] [bit] NOT NULL,
+ CONSTRAINT [PK_Chofer] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Chofer]  WITH CHECK ADD  CONSTRAINT [FK_Chofer_Usuarios] FOREIGN KEY([Id])
+REFERENCES [FSOCIETY].[Usuarios] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Chofer] CHECK CONSTRAINT [FK_Chofer_Usuarios]
+GO
+----------------------------------------
+--Marcas
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Marcas](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Marcas] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+----------------------------------------
+--Modelo
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Modelos](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdMarca] [int] NOT NULL,
+	[Description] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_Modelos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Modelos]  WITH CHECK ADD  CONSTRAINT [FK_Modelos_Marcas] FOREIGN KEY([IdMarca])
+REFERENCES [FSOCIETY].[Marcas] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Modelos] CHECK CONSTRAINT [FK_Modelos_Marcas]
+GO
+----------------------------------------
+--Turnos
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Turnos](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Hora_De_Inicio] [time](0) NOT NULL,
+	[Hora_De_Finalizacion] [time](0) NOT NULL,
+	[Descripcion] [varchar](100) NOT NULL,
+	[Valor_Km] [smallmoney] NOT NULL,
+	[Precio_Base] [smallmoney] NOT NULL,
+	[Habilitado] [bit] NOT NULL,
+ CONSTRAINT [PK_Turnos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+----------------------------------------
+--Autos
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Autos](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Patente] [varchar](8) NOT NULL,
+	[IdModelo] [int] NOT NULL,
+	[IdTurno] [int] NOT NULL,
+	[IdChofer] [int] NOT NULL,
+	[Habilitado] [bit] NOT NULL,
+ CONSTRAINT [PK_Autos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Autos]  WITH CHECK ADD  CONSTRAINT [FK_Autos_Chofer] FOREIGN KEY([IdChofer])
+REFERENCES [FSOCIETY].[Chofer] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Autos] CHECK CONSTRAINT [FK_Autos_Chofer]
+GO
+
+ALTER TABLE [FSOCIETY].[Autos]  WITH CHECK ADD  CONSTRAINT [FK_Autos_Modelos] FOREIGN KEY([IdModelo])
+REFERENCES [FSOCIETY].[Modelos] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Autos] CHECK CONSTRAINT [FK_Autos_Modelos]
+GO
+
+ALTER TABLE [FSOCIETY].[Autos]  WITH CHECK ADD  CONSTRAINT [FK_Autos_Turnos] FOREIGN KEY([IdTurno])
+REFERENCES [FSOCIETY].[Turnos] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Autos] CHECK CONSTRAINT [FK_Autos_Turnos]
+GO
+
+----------------------------------------
+--Cliente
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Cliente](
+	[Id] [int] NOT NULL,
+	[Telefono] [varchar](50) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Codigo_Postal] [varchar](10) NOT NULL,
+	[Habilitado] [bit] NOT NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Cliente]  WITH CHECK ADD  CONSTRAINT [FK_Cliente_Usuarios] FOREIGN KEY([Id])
+REFERENCES [FSOCIETY].[Usuarios] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Cliente] CHECK CONSTRAINT [FK_Cliente_Usuarios]
+GO
+
+
+----------------------------------------
+--Facturacion
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Facturacion](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdCliente] [int] NOT NULL,
+	[IdViaje] [int] NOT NULL,
 	[FechaInicio] [smalldatetime] NOT NULL,
 	[FechaFin] [smalldatetime] NOT NULL,
-	[Importe] [int] NOT NULL,
- CONSTRAINT [PK_FSOCIETY.Facturacion] PRIMARY KEY CLUSTERED 
+	[Importe] [money] NOT NULL,
+ CONSTRAINT [PK_Facturacion] PRIMARY KEY CLUSTERED 
 (
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-/****** Object:  Table [gd_esquema].[FSOCIETY.Rendicion]    Script Date: 4/20/2017 11:24:58 PM ******/
-CREATE TABLE [gd_esquema].[FSOCIETY.Rendicion](
-	[ID] [int] NOT NULL,
-	[IDChofer] [int] NOT NULL,
-	[IDTurno] [int] NOT NULL,
-	[Fecha] [smalldatetime] NOT NULL,
-	[ImporteTotal] [int] NOT NULL,
- CONSTRAINT [PK_FSOCIETY.Rendicion] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
-CREATE TABLE [gd_esquema].[FSOCIETY.Login](
-	[Idusuario] [int] NOT NULL,
+
+ALTER TABLE [FSOCIETY].[Facturacion]  WITH CHECK ADD  CONSTRAINT [FK_Facturacion_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [FSOCIETY].[Cliente] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Facturacion] CHECK CONSTRAINT [FK_Facturacion_Cliente]
+GO
+
+----------------------------------------
+--Rendicion
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Rendicion](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdChofer] [int] NOT NULL,
+	[IdTurno] [int] NOT NULL,
+	[Fecha] [smalldatetime] NOT NULL,
+	[ImporteTotal] [money] NOT NULL,
+ CONSTRAINT [PK_Rendicion] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [FSOCIETY].[Rendicion]  WITH CHECK ADD  CONSTRAINT [FK_Rendicion_Chofer] FOREIGN KEY([IdChofer])
+REFERENCES [FSOCIETY].[Chofer] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Rendicion] CHECK CONSTRAINT [FK_Rendicion_Chofer]
+GO
+
+ALTER TABLE [FSOCIETY].[Rendicion]  WITH CHECK ADD  CONSTRAINT [FK_Rendicion_Turnos] FOREIGN KEY([IdTurno])
+REFERENCES [FSOCIETY].[Turnos] ([Id])
+GO
+
+ALTER TABLE [FSOCIETY].[Rendicion] CHECK CONSTRAINT [FK_Rendicion_Turnos]
+GO
+----------------------------------------
+--Login
+----------------------------------------
+CREATE TABLE [FSOCIETY].[Login](
+	[Idusuario] [nchar](10) NOT NULL,
 	[Reintentos] [smallint] NULL,
 	[Fecha] [date] NOT NULL,
 	[Logeoexitoso] [nchar](2) NOT NULL
 ) ON [PRIMARY]
 
 GO
-CREATE TABLE [gd_esquema].[FSOCIETY.Usuarios](
-	[Idusuario] [int] NOT NULL,
-	[Username] [varchar](30) NOT NULL,
-	[Password] [varbinary](50) NOT NULL,
-  PRIMARY KEY ([Idusuario])
- )ON [PRIMARY]
 
- GO
 
 SET ANSI_PADDING OFF
 GO
