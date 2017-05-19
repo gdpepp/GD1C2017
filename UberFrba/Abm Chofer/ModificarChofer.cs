6aadd722ad03521aacbdd41d5c7a26d6e331241b
Nicolas Perez <nico__1993@hotmail.com>
2017-05-19 19:10:47 -0300

    arreglo perdon x los commit name

---
+++ UberFrba/Abm Chofer/ModificarChofer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba.Abm_Chofer
+{
+    public partial class ModificarChofer : Form
+    {
+        public ModificarChofer()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_volver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bt_guardar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UberFrba/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1 {
+    
+    public sealed class DBConnection {
+
+        string user = ConfigurationManager.AppSettings["user"].ToString();
+        string password = ConfigurationManager.AppSettings["password"].ToString();
+        string server = ConfigurationManager.AppSettings["server"].ToString();
+
+        private static DBConnection instance = new DBConnection();
+
+        public static DBConnection getInstance() {
+            return instance;
+        }
+
+        public SqlConnection getConnection() {
+            SqlConnection dbconn = new SqlConnection();
+            dbconn.ConnectionString = "SERVER=" + server + "\\SQLSERVER2012;DATABASE=GD1C2017;UID=" + user + ";PASSWORD=" + password + ";";
+            return dbconn;
+        }
+    }
+}
+
