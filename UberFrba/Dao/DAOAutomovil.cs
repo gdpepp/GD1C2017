04264b6ecca0ff953c2733841de6f58d462a4cb7
EzequielAG <eze94ale@gmail.com>
2017-05-29 22:22:30 -0300

    (commit provisorio)
    se crea commit para el cambio de branch.

---
+++ UberFrba/Dao/DAOAutomovil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using UberFrba.Utils;
+using UberFrba.Mapping;
+
+namespace UberFrba.Dao
+{
+    class DAOAutomovil
+    {
+        private DataBaseConnector db;
+
+        public DAOAutomovil() {
+            this.db = DataBaseConnector.getInstance();
+        }
+
+        public Automovil getAll() {
+            DataTable dt = db.select_query("SELECT * FROM FSOCIETY.Autos");
+            return new DataTable automoviles;
+        }
+    }
+}
---
+++ UberFrba/Mapping/Automovil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace UberFrba.Mapping
+{
+    class Automovil
+    {
+        private Int32 id;
+        private String patente;
+        private Int32 idModelo;
+        private Int32 idTurno;
+        private Int32 idChofer;
+        private Boolean habilitado;
+
+        public Automovil(Int32 id, String patente, Int32 idModelo, Int32 idTurno, Int32 idChofer, Boolean habilitado) {
+            this.id = id;
+            this.patente = patente;
+            this.idModelo = idModelo;
+            this.idTurno = idTurno;
+            this.idChofer = idChofer;
+            this.habilitado = habilitado;
+        }
+
+        public Automovil(DataRowCollection rows) {
+            foreach (DataRow row in rows)
+            {
+                this.id = Convert.ToInt32(row["Id"]);
+                this.patente = Convert.ToString(row["Patente"]);
+                this.idModelo = Convert.ToInt32(row["IdModelo"]);
+                this.idTurno = Convert.ToInt32(row["IdTurno"]);
+                this.idChofer = Convert.ToInt32(row["IdChofer"]);
+                this.habilitado = Convert.ToBoolean(row["Habilitado"]);
+            }
+        }
+
+        public Int32 getId() {
+            return this.id;
+        }
+
+        public String getPatente() {
+            return this.patente;
+        }
+
+        public Int32 getModelo() {
+            return this.idModelo;
+        }
+
+        public Int32 getTurno()
+        {
+            return this.idTurno;
+        }
+
+        public Int32 getChofer() {
+            return this.idChofer;
+        }
+
+        public Boolean getHabilitado() {
+            return this.habilitado;
+        }
+    }
+}
