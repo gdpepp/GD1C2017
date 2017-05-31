eb75912c029fba5685e8d5ae3c192dbc8cf221b2
Pablo <pnmorrone@gmail.com>
2017-05-31 10:52:24 -0300

    avance de menu

--- UberFrba/Login/SelectProfile.cs
+++ UberFrba/Login/SelectProfile.cs
@@ -52,8 +52,8 @@ namespace UberFrba.Login
 
         private void showMainMenu() {
             UberFrba.Menu.MainMenu menu = new UberFrba.Menu.MainMenu(user);
-            //this.Hide();//reimplementar con un close.
-            menu.ShowDialog();
+            this.Hide();//reimplementar con un close.
+            menu.Show();
             //this.Close();
             
         }
--- UberFrba/Menu/MainMenu.cs
+++ UberFrba/Menu/MainMenu.cs
@@ -8,18 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UberFrba.Mapping;
+using UberFrba.Dao;
 
 namespace UberFrba.Menu
 {
     public partial class MainMenu : Form
     {
         private Usuario user;
+        private List<Funcionalidades> functions;
 
         public MainMenu(Object user)
         {
             this.user = (Usuario)user;
+            setupFunctions();
             InitializeComponent();
             this.label1.Text = "Bienvenido " + this.user.getUsername() + " con perfil: " + this.user.getRol().getDescription();
+        }
+
+        private void setupFunctions(){
+            DaoFuncionalidades dao = new DaoFuncionalidades();
+            this.functions = dao.getFunctionsByRol(user.getRol());
+        }
+
+        private void setupMenu(){
+           
         }
     }
 }
