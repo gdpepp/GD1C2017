f1211142f373579db08192795611b49d8423546b
Pablo <pnmorrone@gmail.com>
2017-06-09 20:11:22 -0300

    arreglo menu principal

--- UberFrba/Menu/MainMenu.Designer.cs
+++ UberFrba/Menu/MainMenu.Designer.cs
@@ -32,7 +32,7 @@
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(823, 442);
             this.Margin = new System.Windows.Forms.Padding(2);
