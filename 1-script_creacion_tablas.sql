476a0eb2c75cf84cdcb5a0a16396aab07c693935
EzequielAG <eze94ale@gmail.com>
2017-06-19 16:25:08 -0300

    Agrego la funcionalidad.
    NO MERGE.
    FALTA UNA ACTUALIZACION A LA ULTIMA CONSULTA Y ALGUNAS MEJORAS AL FORM.

--- 1-script_creacion_tablas.sql
+++ 1-script_creacion_tablas.sql
@@ -531,7 +531,7 @@ VALUES('Clientes',NULL,NULL),('Choferes',NULL,NULL),('Autos',NULL,NULL),
 	  ('Alta Cliente','ABMCliente',1),('Baja Cliente',NULL,1),('Alta Chofer','ABMChofer',2),
 	  ('Consultas Autos','Automovil',3),('Roles',NULL,NULL),('Abm Roles','AbmRol',8),
 	  ('Viajes',NULL,NULL),('Registrar Viaje','Viaje',10),('Rendicion de Viaje','RendicionViaje',10),
-	  ('Facturacion','Facturacion',10);
+	  ('Facturacion','Facturacion',10),('Listados',NULL,NULL),('Consulta','ListadoEstadistico',14);
 
 GO
 
